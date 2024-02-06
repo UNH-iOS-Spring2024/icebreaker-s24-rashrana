e2c386d03c52b11739e8c3a5bbf109d53da6c991
Prashant Rana <rashrana.pr@gmail.com>
2024-02-06 18:06:13 -0500

    add files

---
+++ Icebreajer_S24_Rash/Icebreajer_S24_Rash/Icebreajer_S24_RashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Icebreajer_S24_RashApp.swift
+//  Icebreajer_S24_Rash
+//
+//  Created by Prashant Rana on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Icebreajer_S24_RashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
