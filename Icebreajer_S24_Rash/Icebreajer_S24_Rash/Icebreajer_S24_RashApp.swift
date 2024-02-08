b5d006563e833e55b77c52209d5ac0f7ba8ff1c7
Prashant Rana <rashrana.pr@gmail.com>
2024-02-08 12:26:00 -0500

    Connected with Firebase

--- Icebreajer_S24_Rash/Icebreajer_S24_Rash/Icebreajer_S24_RashApp.swift
+++ Icebreajer_S24_Rash/Icebreajer_S24_Rash/Icebreajer_S24_RashApp.swift
@@ -6,9 +6,30 @@
 //
 
 import SwiftUI
+import Firebase
+
+
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//}
+
 
 @main
 struct Icebreajer_S24_RashApp: App {
+    // register app delegate for Firebase setup
+//      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    init() {
+        let providerFactory = AppCheckDebugProviderFactory()
+        AppCheck.setAppCheckProviderFactory(providerFactory)
+        
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
